175b48e670ddeabe2f84e50cf68d8e2c288bbbf4
mlo <mlo@dandomain.dk>
2016-08-11 22:52:03 +0200

    removed default bloat and made application really simple.

--- Webhooks/Controllers/HomeController.cs
+++ Webhooks/Controllers/HomeController.cs
@@ -1,30 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.IO;
 using System.Web.Mvc;
+using System.Windows.Forms;
 
 namespace Webhooks.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpPost]
         public ActionResult Index()
         {
-            return View();
-        }
+            using (var reader = new StreamReader(Request.InputStream))
+            {
+                var text = reader.ReadToEnd();
+                MessageBox.Show(text);
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+                return Content(text);
+            }
         }
     }
 }
--- Webhooks/Startup.cs
+++ Webhooks/Startup.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartupAttribute(typeof(Webhooks.Startup))]
+[assembly: OwinStartup(typeof(Webhooks.Startup))]
 namespace Webhooks
 {
-    public partial class Startup
+    internal class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
